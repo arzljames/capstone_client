dff706ba4ed179b6a296d93b2908479670c7dac3
Arzl James <arzljames15@gmail.com>
2022-05-28 06:28:04 +0800

    sds

--- src/App.js
+++ src/App.js
@@ -67,6 +67,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/login" element={<Login />} />
       {/* <Route
         path="/account/verification/:code/:id"
         element={<VerificationPage />}
