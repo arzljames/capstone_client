0bcccdb501099fbd9080e76f3a711333c0987584
Arzl James <arzljames15@gmail.com>
2022-05-28 10:45:28 +0800

    sds

--- src/App.js
+++ src/App.js
@@ -80,6 +80,12 @@ function App() {
             <Route path="/notifications" element={<Notification />} />
             <Route path="/chat/:userId/:id" element={<ChatUser />} />
           </Route>
+
+          <Route element={<ProtectedLoginRoutes user={user} />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="reset-password" element={<ForgotPassword />} />
+          </Route>
           {/* <Route
             path="account/verification/:code/:id"
             element={<VerificationPage />}
