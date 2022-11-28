a4a58a0df8343b8e9ca0d4b8e3c50cc45f131b03
Arzl James <70579069+arzljames@users.noreply.github.com>
2022-11-28 14:54:57 +0800

    update API server

--- src/API/Api.js
+++ src/API/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 axios.defaults.withCredentials = true;
 
 
-let serverUrl = 'https://zcmc.herokuapp.com/'
+let serverUrl = 'https://telemedicine.ap-1.evennode.com/'
 
 //Custom API to fetch data from the server using axios
 const api = axios.create({
