eab17844a439c31f12799a236cde8b43c859c90b
Arzl James <70579069+arzljames@users.noreply.github.com>
2022-12-15 18:43:42 +0800

    update api server in socket component

--- src/Components/Socket.js
+++ src/Components/Socket.js
@@ -2,4 +2,4 @@ import io from "socket.io-client";
 
 //https://zcmc.herokuapp.com/
 
-export const socket = io.connect("https://zcmc.herokuapp.com/");
+export const socket = io.connect("https://telemedicine.ap-1.evennode.com/");
